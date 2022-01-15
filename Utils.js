ddcab492199bbc9024b002ee745dbd8ca66904c4
Riccardo Basso <ricba1995@gmail.com>
2022-01-15 16:12:59 +0100

    added converter ALGO to $

--- Utils.js
+++ Utils.js
@@ -9,8 +9,8 @@ class Utils {
         return microAlgos / 1000000;
     }
 
-    static printOperationFee(operation_fee, total_new_fee, reason) {
-        console.log(`Added fee of ${operation_fee} microAlgos (${this.microToAlgo(operation_fee)} ALGO) for ${reason}. Total amount is now ${total_new_fee} (${this.microToAlgo(total_new_fee)} ALGO -> ${ALGORAND_CURRENT_PRICE} $).`)
+    static printOperationFee(operation_fee, total_new_fee, reason, ALGORAND_CURRENT_PRICE) {
+        console.log(`Added fee of ${operation_fee} microAlgos (${this.microToAlgo(operation_fee)} ALGO) for ${reason}. Total amount is now ${total_new_fee} (${this.microToAlgo(total_new_fee)} ALGO -> ${ALGORAND_CURRENT_PRICE * Utils.microToAlgo(total_new_fee)} $).`)
     }
 
     static async getCryptoCurrentInfo(cryptoID) {
--- algoCalc.js
+++ algoCalc.js
@@ -12,7 +12,7 @@ const receiversCount = args[1];
 const freeze = args[2];
 
 (async () => {
-    var ALGORAND_CURRENT_PRICE = (await Utils.getCryptoCurrentInfo('alg')).ticker.price;
+    const ALGORAND_CURRENT_PRICE = parseFloat((await Utils.getCryptoCurrentInfo('alg')).ticker.price);
     console.log(ALGORAND_CURRENT_PRICE);
 
     if (!assetsCount || !receiversCount) {
@@ -28,37 +28,37 @@ const freeze = args[2];
     // MINT ASSET FEE - Increase fee for each asset that we want to create. It does not care what is the total supply for the single asset.
     const asset_mint_fee = gasFee * assetsCount;
     total_fee += asset_mint_fee;
-    Utils.printOperationFee(asset_mint_fee, total_fee, `mint of ${assetsCount} ${assetsCount > 1 ? 'assets' : 'asset'}`);
+    Utils.printOperationFee(asset_mint_fee, total_fee, `mint of ${assetsCount} ${assetsCount > 1 ? 'assets' : 'asset'}`, ALGORAND_CURRENT_PRICE);
     
     
     // TRANSFER FUNDS TO REACH MIN BALANCE FOR RECEIVERS - Increase fee for each transfer of microAlgos equal to minimum balance that is needed to set minimum account balance to receive assets. Need to add also tx fee.
     // MINBAL_number is fixed to 100000 microAlgos to have an account valid + 100000 for each asset we have, it means that to receive an asset is required 200000 microAlgos in the balance
     const account_min_balance_transfer = ((minimum_balance + minimum_balance) + gasFee) * receiversCount;
     total_fee += account_min_balance_transfer;
-    Utils.printOperationFee(account_min_balance_transfer, total_fee, `transfering minimum balance needed to receive the asset`);
+    Utils.printOperationFee(account_min_balance_transfer, total_fee, `transfering minimum balance needed to receive the asset`, ALGORAND_CURRENT_PRICE);
     
     
     // OPT IN FOR RECEIVERS - Increase fee for each opt in tx that must be done to opt in for the asset. It is multiplied by the number of receivers.
     const receivers_opt_in = gasFee * receiversCount;
     total_fee += receivers_opt_in;
-    Utils.printOperationFee(receivers_opt_in, total_fee, `opting in ${receiversCount} ${receiversCount > 1 ? 'receivers' : 'receiver'} to the asset`);
+    Utils.printOperationFee(receivers_opt_in, total_fee, `opting in ${receiversCount} ${receiversCount > 1 ? 'receivers' : 'receiver'} to the asset`, ALGORAND_CURRENT_PRICE);
     
     
     // ASSET TRANSFER - Increase fee for each transfer of the asset.
     const transfer_asset = gasFee * receiversCount;
     total_fee += transfer_asset;
-    Utils.printOperationFee(transfer_asset, total_fee, `transfering ${assetsCount} ${assetsCount > 1 ? 'assets' : 'asset'} to ${receiversCount} ${receiversCount > 1 ? 'receivers' : 'receiver'}`);
+    Utils.printOperationFee(transfer_asset, total_fee, `transfering ${assetsCount} ${assetsCount > 1 ? 'assets' : 'asset'} to ${receiversCount} ${receiversCount > 1 ? 'receivers' : 'receiver'}`, ALGORAND_CURRENT_PRICE);
     
     
     // (optional) FREEZE ASSET TO RECEIVERS - it is possible that the creator wants to freeze the asset sent to receiver. This is another tx cost to consider.
     if (freeze === 'true') {
         const freeze_asset = gasFee * receiversCount;
         total_fee += freeze_asset;
-        Utils.printOperationFee(freeze_asset, total_fee, `freezing asset to ${receiversCount} ${receiversCount > 1 ? 'receivers' : 'receiver'}`);
+        Utils.printOperationFee(freeze_asset, total_fee, `freezing asset to ${receiversCount} ${receiversCount > 1 ? 'receivers' : 'receiver'}`, ALGORAND_CURRENT_PRICE);
     }
     
     
     
     // TOTAL
-    console.log(`\n\nTotal fee: ${total_fee} microalgos (${Utils.microToAlgo(total_fee)} ALGO)`);
+    console.log(`\n\nTotal fee: ${total_fee} microalgos (${Utils.microToAlgo(total_fee)} ALGO -> ${ALGORAND_CURRENT_PRICE * Utils.microToAlgo(total_fee)} $)`);
 })();
